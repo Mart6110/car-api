3a4769ad71fa2e03bc5045f3bcdc60241647cdf8
Mart6110 <mart6110.code@gmail.com>
2024-02-15 10:06:03 +0100

    first commit

---
+++ car.model.js
@@ -0,0 +1,11 @@
+class Car {
+    constructor(id, rank, model, quantity, changeQuantityPercent) {
+        this.id = id;
+        this.rank = rank;
+        this.model = model;
+        this.quantity = quantity;
+        this.changeQuantityPercent = changeQuantityPercent;
+    }
+}
+
+module.exports = Car;
